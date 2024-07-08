a69f6c0d889bdd625bc885e4a97f37c8853bd72c
shakibdshy <shakibdshy@gmail.com>
2024-07-08 23:40:27 +0600

    Configure Callback with custom session and JWT handling

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,41 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "../auth.config";
 import { db } from "@/utils/db";
+import { getUserById } from "./data/user";
+import { Role } from "@prisma/client";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: Role;
+    } & DefaultSession["user"]
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
+          role: token.role,
+        }
+      };
+    },
+    async jwt({ token }) {
+      if(!token.sub) return token;
+
+      const existingUser =  await getUserById(token.sub);
+      if(!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
