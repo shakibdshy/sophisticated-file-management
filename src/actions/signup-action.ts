ad8d021708d5d50865a3cd25e02bd72d01e2ac7a
shakibdshy <shakibdshy@gmail.com>
2024-07-08 22:51:52 +0600

    Implement signup action & Configure routes

---
+++ src/actions/signup-action.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import bcrypt from "bcryptjs";
+import { signUpSchema } from "@/schemas/auth.schema";
+import * as z from "zod";
+import { db } from "@/utils/db";
+import { getUserByEmail } from "@/data/user";
+
+export const signUpAction = async (values: z.infer<typeof signUpSchema>) => {
+  const validatedFields = signUpSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid Credentials!" };
+  }
+
+  const {fName, lName, email, password} = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User already exists!" };
+  }
+
+  await db.user.create({
+    data: {
+      name: fName + " " + lName,
+      email,
+      password: hashedPassword,
+    },
+    include: {
+      accounts: true,
+    },
+  });
+
+  return { success: "Successfully User Created!" };
+};
--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {
+  Alert,
   Box,
   Button,
   Card,
@@ -15,6 +16,11 @@ import {
 } from "@mui/material";
 import SocialIcon from "./social-icon";
 import Link from "next/link";
+import { useState, useTransition } from "react";
+import { SubmitHandler } from "react-hook-form";
+import { signUpSchema, SignUpSchema } from "@/schemas/auth.schema";
+import { Form } from "../ui/form";
+import { signUpAction } from "@/actions/signup-action";
 
 const Root = styled("div")(({ theme }) => ({
   width: "100%",
@@ -25,24 +31,34 @@ const Root = styled("div")(({ theme }) => ({
   },
 }));
 
+const initialValues: SignUpSchema = {
+  fName: "",
+  lName: "",
+  email: "",
+  password: "",
+};
+
 export default function SignUpForm() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+  const [isError, setError] = useState<string | undefined>("");
+  const [isSuccess, setSuccess] = useState<string | undefined>("");
+  const [isPending, setIsPending] = useTransition();
+
+  const onSubmit: SubmitHandler<SignUpSchema> = (data) => {
+    setIsPending(() => {
+      signUpAction(data).then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      });
     });
   };
-
   return (
     <Card sx={{ maxWidth: 500, margin: "auto", boxShadow: 0, borderRadius: 5 }}>
       <CardContent>
         <Stack spacing={2} alignItems="center">
           <CardHeader
             className="text-center"
-            title="Welcome Back"
-            subheader="Enter your credentials to access your account"
+            title="Create an Account"
+            subheader="Enter your details to create an account"
             classes={{
               title: "text-3xl font-bold mb-3",
             }}
@@ -53,96 +69,115 @@ export default function SignUpForm() {
               <Typography fontSize={16}>Or continue with</Typography>
             </Divider>
           </Root>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+          <Form<SignUpSchema>
+            validationSchema={signUpSchema}
+            onSubmit={onSubmit}
+            useFormProps={{
+              mode: "onChange",
+              defaultValues: initialValues,
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
+            {({ register, formState: { errors } }) => (
+              <Stack spacing={2} alignItems="center">
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      {...register("fName")}
+                      error={!!errors.fName}
+                      helperText={errors.fName?.message}
+                      disabled={isPending}
+                      autoComplete="given-name"
+                      fullWidth
+                      label="First Name"
+                      autoFocus
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          minHeight: 56,
+                        },
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      {...register("lName")}
+                      error={!!errors.lName}
+                      helperText={errors.lName?.message}
+                      disabled={isPending}
+                      fullWidth
+                      label="Last Name"
+                      autoComplete="family-name"
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          minHeight: 56,
+                        },
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      {...register("email")}
+                      error={!!errors.email}
+                      helperText={errors.email?.message}
+                      disabled={isPending}
+                      fullWidth
+                      label="Email Address"
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          minHeight: 56,
+                        },
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      {...register("password")}
+                      error={!!errors.password}
+                      helperText={errors.password?.message}
+                      disabled={isPending}
+                      fullWidth
+                      label="Password"
+                      type="password"
+                      sx={{
+                        "& .MuiInputBase-root": {
+                          minHeight: 56,
+                        },
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+                {isSuccess && (
+                  <Alert severity="success" sx={{ width: "100%" }}>
+                    {isSuccess}
+                  </Alert>
+                )}
+                {isError && (
+                  <Alert severity="error" sx={{ width: "100%" }}>
+                    {isError}
+                  </Alert>
+                )}
+                <Button
+                  type="submit"
                   fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  sx={{
-                    "& .MuiInputBase-root": {
-                      minHeight: 56,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                  sx={{
-                    "& .MuiInputBase-root": {
-                      minHeight: 56,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  sx={{
-                    "& .MuiInputBase-root": {
-                      minHeight: 56,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  sx={{
-                    "& .MuiInputBase-root": {
-                      minHeight: 56,
-                    },
-                  }}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              size="large"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link
-                  href="/auth/signin"
-                  className="no-underline text-green-700 hover:underline"
+                  variant="contained"
+                  size="large"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={isPending}
                 >
-                  Already have an account? Sign In
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+                  Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link
+                      href="/auth/signup"
+                      className="no-underline text-green-700 hover:underline"
+                    >
+                      Already have an account? Sign In
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Stack>
+            )}
+          </Form>
         </Stack>
       </CardContent>
     </Card>
