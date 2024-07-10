207de2c9005d0de78017ec800fc63e977aca6886
shakibdshy <shakibdshy@gmail.com>
2024-07-10 23:18:57 +0600

    Implementing user base file upload

--- src/actions/file-action.ts
+++ src/actions/file-action.ts
@@ -2,13 +2,31 @@
 
 import { auth } from "@/auth";
 import { db } from "@/utils/db";
+import { revalidatePath } from "next/cache";
+
+export const getFiles = async () => {
+  const user = await auth();
+  if (!user) throw new Error("User not found");
+  
+  const files = await db.file.findMany({
+    where: {
+      userId: user.user.id,
+    },
+    include: {
+      user: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return files;
+};
 
 export const uploadFile = async (inputs: any) => {
   const user = await auth();
-  console.log(inputs);
 
   if (!user) throw new Error("User not found");
-
 
   try {
     await db.file.create({
@@ -23,4 +41,6 @@ export const uploadFile = async (inputs: any) => {
   } catch (error) {
     console.log(error);
   }
+
+  revalidatePath("/dashboard/file");
 };
