2d969f75d7e47ad5fc5f69a4e1fd329cba2f4e2e
shakibdshy <shakibdshy@gmail.com>
2024-07-12 21:04:20 +0600

    Improving the whiteboard for dynamic import

--- src/app/preview/[id]/page.tsx
+++ src/app/preview/[id]/page.tsx
@@ -1,18 +1,5 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { Room } from "@/app/shared/room";
-import Live from "@/components/liveblocks/live";
-import Navbar from "@/components/liveblocks/navbar";
+const App = dynamic(() => import("@/app/shared/app"), { ssr: false });
 
-export default function page({ params }: { params: { id: string } }) {
-  return (
-    <Room>
-      <div className="h-screen overflow-hidden">
-        <Navbar />
-        <section className="flex h-full flex-row">
-          <Live />
-        </section>
-      </div>
-    </Room>
-  );
-}
+export default App;
