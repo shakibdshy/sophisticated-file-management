0dec8b124b6a0ba0b7f282c5bcd53dc440936af3
shakibdshy <shakibdshy@gmail.com>
2024-07-12 20:54:54 +0600

    modify the global config file

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,6 +1,6 @@
 // Define Liveblocks types for your application
 
-import { LiveList } from "@liveblocks/client";
+import { LiveList, LiveMap } from "@liveblocks/client";
 
 // https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 declare global {
@@ -8,13 +8,13 @@ declare global {
     // Each user's Presence, for useMyPresence, useOthers, etc.
     Presence: {
       // Example, real-time cursor coordinates
-      cursor: { x: number; y: number, message: string; };
+      // cursor: { x: number; y: number, message: string; };
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
       // Example, a conflict-free list
-      animals: LiveList<string>;
+      canvasObjects: LiveMap<string, any>;
     };
 
     // Custom user info set when authenticating with a secret key
@@ -41,6 +41,8 @@ declare global {
       // Example, attaching coordinates to a thread
       x: number;
       y: number;
+      zIndex: number;
+      resolved: boolean;
     };
 
     // Custom room info set with resolveRoomsInfo, for useRoomInfo
