8ef37086b2b771f4eea6b7f39914f855fd6b5c0d
shakibdshy <shakibdshy@gmail.com>
2024-07-12 20:57:58 +0600

    Improving the Loading animation

--- src/app/shared/room.tsx
+++ src/app/shared/room.tsx
@@ -1,22 +1,30 @@
-"use client";
+// "use client";
 
-import { ReactNode } from "react";
-import {
-  LiveblocksProvider,
-  RoomProvider,
-  ClientSideSuspense,
-} from "@liveblocks/react/suspense";
+// import { ReactNode } from "react";
+// import {
+//   LiveblocksProvider,
+//   RoomProvider,
+//   ClientSideSuspense,
+// } from "@liveblocks/react/suspense";
+// import { LiveMap } from "@liveblocks/client";
+// import Loader from "@/components/liveblocks/Loader";
 
-export function Room({ children }: { children: ReactNode }) {
-  return (
-    <LiveblocksProvider
-      publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY!}
-    >
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
-          {children}
-        </ClientSideSuspense>
-      </RoomProvider>
-    </LiveblocksProvider>
-  );
-}
+// export function Room({ children }: { children: ReactNode }) {
+//   return (
+//     <LiveblocksProvider
+//       publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY!}
+//     >
+//       <RoomProvider
+//         id="my-room"
+//         initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
+//         initialStorage={{
+//           canvasObjects: new LiveMap(),
+//         }}
+//       >
+//         <ClientSideSuspense fallback={<Loader />}>
+//           {children}
+//         </ClientSideSuspense>
+//       </RoomProvider>
+//     </LiveblocksProvider>
+//   );
+// }
