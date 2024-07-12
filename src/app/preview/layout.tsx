a8a381c8bafadc3dd480b560e2b210a92ce40448
shakibdshy <shakibdshy@gmail.com>
2024-07-12 20:58:43 +0600

    Improving the global layout for the whiteboard

---
+++ src/app/preview/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { LiveMap } from "@liveblocks/client";
+import Loader from "@/components/liveblocks/Loader";
+
+export default function RoomLayout({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider
+      publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY!}
+    >
+      <RoomProvider
+        id="my-room"
+        initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
+        initialStorage={{
+          canvasObjects: new LiveMap(),
+        }}
+      >
+        <ClientSideSuspense fallback={<Loader />}>
+          {() => children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
